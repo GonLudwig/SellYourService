ef8ca3077d52c7ec629e45e8a83688322cf059b7
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-02-03 16:08:54 -0300

    add fillable

--- app/app/Models/Schedule.php
+++ app/app/Models/Schedule.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Schedule extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'product_id',
+        'period_id',
+        'client_id',
+        'check_in',
+        'check_out',
+        'confirmation'
+    ];
 }
