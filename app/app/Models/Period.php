8597f84ed0312b22ef5f37b7386e8d8a3efe5164
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-02-03 16:05:51 -0300

    add fillable

--- app/app/Models/Period.php
+++ app/app/Models/Period.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Period extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'started_at',
+        'ended_at'
+    ];
 }
