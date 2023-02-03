11e61c150228a182bdb212cae77005b131a80c84
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-02-03 15:51:42 -0300

    create Period

---
+++ app/database/migrations/2023_02_03_171551_create_periods_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('periods', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->timestamp('started_at');
+            $table->timestamp('ended_at', );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('periods');
+    }
+};
