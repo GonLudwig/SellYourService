917f2b7c154cd7851d17c67eae4e86ebecdbe07e
GlaucoStarling <glaucox.x@gmail.com>
2023-02-03 22:03:29 -0300

    Create update validation

--- app/app/Http/Requests/UpdatePriceRequest.php
+++ app/app/Http/Requests/UpdatePriceRequest.php
@@ -13,7 +13,7 @@ class UpdatePriceRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,24 @@ class UpdatePriceRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name'          => 'required|max:50',
+            'description'   => 'max:200',
+            'price'         => 'required|decimal:2'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'required'          => 'O campo :attribute é obrigatorio!',
+            'name.max'          => 'O campo deve possuir no maximo 50 caracteres.',
+            'description.max'   => 'O campo deve possuir no maximo 200 caracteres.',
+            'price.decimal'     => 'O campo precisa possuir apenas duas casas decimais.'
         ];
     }
 }
