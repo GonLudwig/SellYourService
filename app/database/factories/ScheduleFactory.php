aa43574f32c753d4dfe63b538a5ad23d61d814f0
GlaucoStarling <glauco.pereira@versatecnologia.com.br>
2023-02-08 17:02:22 -0300

    Create a schedule seeder

--- app/database/factories/ScheduleFactory.php
+++ app/database/factories/ScheduleFactory.php
@@ -17,7 +17,12 @@ class ScheduleFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'product_id' => fake()->numberBetween(1, 10),
+            'period_id' => fake()->numberBetween(1, 10),
+            'client_id' => fake()->numberBetween(1, 10),
+            'check_in' => fake()->dateTimeBetween('-1 week'),
+            'check_out' => fake()->dateTimeBetween('now', '+1 week'),
+            'confirmation' => fake()->boolean()
         ];
     }
 }
--- app/database/seeders/ScheduleSeeder.php
+++ app/database/seeders/ScheduleSeeder.php
@@ -14,6 +14,6 @@ class ScheduleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Schedule::factory(10)->create();
     }
 }
